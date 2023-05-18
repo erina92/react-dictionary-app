e7ee116839c8dc6572376165f68694f4b02a28f8
erina92 <erikamiglietta92@gmail.com>
2023-05-18 19:18:36 +0200

    created search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+    <div className="App">
+      <div className="container mx-auto max-w-3xl bg-slate-900 rounded-xl shadow border p-8 m-10">
+        <Search />
+      </div>
     </div>
   );
 }
