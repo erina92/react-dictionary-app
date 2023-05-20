681f04ba9dd883316140ec689ff4a78628d78f37
erina92 <erikamiglietta92@gmail.com>
2023-05-20 11:10:33 +0200

    fixed some css

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,15 +4,17 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3 className="underline decoration-pink-500 capitalize mt-2">
+      <h3 className="underline decoration-pink-600 mt-2">
         {props.meaning.partOfSpeech}
       </h3>
       {props.meaning.definitions.map(function (definition, index) {
-        if (index < 1) {
+        if (index < 3) {
           return (
             <div key={index} className="text-justify">
               <p className="mt-2">{definition.definition}</p>
-              <p className="italic text-sm mt-2">{definition.example}</p>
+              <p className="italic text-sm mt-2">
+                <span className="text-pink-500">{definition.example}</span>
+              </p>
             </div>
           );
         } else {
